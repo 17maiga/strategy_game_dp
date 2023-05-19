c1b6d664d05ed01b28a4c4414668b822f5fc5481
Henri Saudubray <henri.saudubray@gmail.com>
2023-05-19 22:24:03 +0200

    Move Map to WorldMap to avoid confusion and implement

---
+++ app/src/main/java/strategy_game_dp/world/WorldMap.java
@@ -0,0 +1,45 @@
+package strategy_game_dp.world;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+import strategy_game_dp.producible.unit.Unit;
+
+public record WorldMap(int width, int height, List<List<Cell>> cells) {
+  private static WorldMap instance = null;
+
+  public static WorldMap getInstance(int width, int height) {
+    if (instance == null) {
+      List<List<Cell>> cells = new ArrayList<>();
+      for (int i = 0; i < height; i++) {
+        List<Cell> row = new ArrayList<>();
+        for (int j = 0; j < width; j++) {
+          Cell cell = new Cell(j, i);
+          row.add(j, cell);
+        }
+        cells.add(i, row);
+      }
+      instance = new WorldMap(width, height, cells);
+    }
+    return instance;
+  }
+
+  @Contract(pure = true)
+  public static WorldMap getInstance() {
+    if (instance == null) {
+      throw new UnsupportedOperationException();
+    }
+    return instance;
+  }
+
+  public Cell getCell(final int x, final int y) {
+    return cells.get(y).get(x);
+  }
+
+  public void insertUnits(@NotNull List<Unit> units) {
+    for (Unit unit : units) {
+      cells.get(unit.getY()).get(unit.getX()).insertUnit(unit);
+    }
+  }
+}
