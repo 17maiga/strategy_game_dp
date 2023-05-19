c0a296304b4f9a62a2bf871b5fc5be0dd6a04f00
Henri Saudubray <henri.saudubray@gmail.com>
2023-05-19 22:22:15 +0200

    Implement Inventory class

---
+++ app/src/main/java/strategy_game_dp/world/Inventory.java
@@ -0,0 +1,36 @@
+package strategy_game_dp.world;
+
+import org.jetbrains.annotations.Contract;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Inventory {
+  private final Map<ResourceType, Integer> resources;
+
+  private static Inventory instance;
+
+  @Contract(pure = true)
+  public Inventory() {
+    this.resources = new HashMap<>();
+  }
+
+  public void add(final ResourceType type, final int amount) {
+    resources.put(type, resources.getOrDefault(type, 0) + amount);
+  }
+
+  public void remove(final ResourceType type, final int amount) {
+    resources.put(type, resources.getOrDefault(type, 0) - amount);
+  }
+
+  public int get(final ResourceType type) {
+    return resources.getOrDefault(type, 0);
+  }
+
+  public static Inventory getInstance() {
+    if (instance == null) {
+      instance = new Inventory();
+    }
+    return instance;
+  }
+}
