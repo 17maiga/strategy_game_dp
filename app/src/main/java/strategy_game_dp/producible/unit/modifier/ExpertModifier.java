62c1b55b7fa3384ff2e20164ec1b6e0e9426c0d0
Henri Saudubray <henri.saudubray@gmail.com>
2023-05-20 10:23:54 +0200

    Expert & Rider modifiers

---
+++ app/src/main/java/strategy_game_dp/producible/unit/modifier/ExpertModifier.java
@@ -0,0 +1,67 @@
+package strategy_game_dp.producible.unit.modifier;
+
+import org.jetbrains.annotations.Contract;
+
+public class ExpertModifier extends UnitModifier {
+
+  private int deactivateCountdown;
+
+  @Contract(pure = true)
+  public ExpertModifier() {
+    deactivateCountdown = 5;
+  }
+
+  @Override
+  public boolean isEfficiencyMultiplier() {
+    return true;
+  }
+
+  @Override
+  @Contract(pure = true)
+  public int updateEfficiency(int efficiency) {
+    return efficiency * 2;
+  }
+
+  @Override
+  public boolean isSpeedMultiplier() {
+    return true;
+  }
+
+  @Override
+  @Contract(pure = true)
+  public int updateSpeed(int speed) {
+    return (int) Math.floor(speed * 1.5);
+  }
+
+  @Override
+  public boolean isHungerMultiplier() {
+    return true;
+  }
+
+  @Override
+  @Contract(pure = true)
+  public int updateHunger(int hunger) {
+    return 2 * hunger;
+  }
+
+  @Override
+  public void update() {
+    if (unit == null) {
+      active = false;
+      return;
+    }
+    if (active) {
+      if (unit.canMine()) {
+        deactivateCountdown = 5;
+      } else {
+        deactivateCountdown--;
+        if (deactivateCountdown == 0) {
+          active = false;
+          deactivateCountdown = 5;
+        }
+      }
+    } else if (unit.getXp() > 100 && unit.canMine()) {
+      active = true;
+    }
+  }
+}
---
+++ app/src/main/java/strategy_game_dp/producible/unit/modifier/RiderModifier.java
@@ -0,0 +1,33 @@
+package strategy_game_dp.producible.unit.modifier;
+
+public class RiderModifier extends UnitModifier {
+  @Override
+  public boolean isSpeedMultiplier() {
+    return true;
+  }
+
+  @Override
+  public int updateSpeed(int speed) {
+    return 2 * speed;
+  }
+
+  @Override
+  public int updateHunger(int hunger) {
+    return hunger + 3;
+  }
+
+  @Override
+  public void update() {
+    if (unit == null) {
+      active = false;
+      return;
+    }
+    if (active && !unit.canMine()) {
+      active = false;
+    } else {
+      if (unit.canMine()) {
+        active = true;
+      }
+    }
+  }
+}
