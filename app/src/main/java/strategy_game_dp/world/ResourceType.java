5c857617fff7774a7558371de2a442ad9ef815bc
Henri Saudubray <henri.saudubray@gmail.com>
2023-05-20 09:50:42 +0200

    Resource types

--- app/src/main/java/strategy_game_dp/world/ResourceType.java
+++ app/src/main/java/strategy_game_dp/world/ResourceType.java
@@ -1,8 +1,34 @@
 package strategy_game_dp.world;
 
+import org.jetbrains.annotations.Contract;
+
 public enum ResourceType {
-    Wood,
-    Stone,
-    Gold,
-    Food,
+  WOOD(0.3, 50),
+  STONE(0.3, 50),
+  GOLD(0.3, 50),
+  FOOD(0.3, 50);
+
+  private final double spawnChance;
+  private final int maxVeinSize;
+
+  @Contract(pure = true)
+  ResourceType(double spawnChance, int maxVeinSize) {
+    this.spawnChance = spawnChance;
+    this.maxVeinSize = maxVeinSize;
+  }
+
+  @Contract(pure = true)
+  public static ResourceType getRandomType() {
+    return WOOD;
+  }
+
+  @Contract(pure = true)
+  public double getSpawnChance() {
+    return spawnChance;
+  }
+
+  @Contract(pure = true)
+  public int getMaxVeinSize() {
+    return maxVeinSize;
+  }
 }
